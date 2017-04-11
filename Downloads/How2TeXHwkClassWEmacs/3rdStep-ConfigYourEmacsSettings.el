f30ac4533c799fe2b359985484b3fba65694955a
pathologicalhandwaving <kshort@iastate.edu>
2017-04-11 12:22:30 -0400

    notes

--- Downloads/How2TeXHwkClassWEmacs/3rdStep-ConfigYourEmacsSettings.el
+++
@@ -1,9 +0,0 @@
-; finally you should add your own class into the variable `org-latex-classes'
-  (add-to-list 'org-latex-classes
-    ("networkHW" "\\documentclass{networkHW}" ;; ONLY this line matters, the rest is just format controlling, just leave them be
-    ("\\section{%s}" . "\\section*{%s}")
-    ("\\subsection{%s}" . "\\subsection*{%s}")
-    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-    ("\\paragraph{%s}" . "\\paragraph*{%s}")
-    ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-    )
